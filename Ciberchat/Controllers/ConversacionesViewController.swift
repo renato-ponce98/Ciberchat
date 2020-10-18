0ee211869c2075913a4784d87752b16c4c03b3c7
Renato Ponce <69768980+renato-ponce98@users.noreply.github.com>
2020-10-17 20:53:58 -0500

    video 01

--- Ciberchat/Controllers/ConversacionesViewController.swift
+++ Ciberchat/Controllers/ConversacionesViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ConversacionesViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ Ciberchat/Controllers/ReproductorVideoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ReproductorVideoViewController.swift
+//  Ciberchat
+//
+//  Created by user182813 on 10/17/20.
+//
+
+import UIKit
+
+class ReproductorVideoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
